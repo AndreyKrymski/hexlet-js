f9c625f5397249577ecb5cd42a02d4f869303dcf
Andrey Krymski <6714223@mail.ru>
2022-04-09 21:10:59 +0300

    Зависимости

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+import fs from "fs";
+import _ from "lodash";
 console.log("Hello, Hexlet!");
 console.log(Math.sqrt(256) + 100);
-
+console.log(Math.round(5.34));
+console.log(_.intersection([2, 1], [2, 3]));
+console.log(_.capitalize("hello"));
+console.log(_.last(['one', 'two']));
